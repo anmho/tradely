9bc57a3ea9f61821f175d9c95026ff7beccf77fb
Andrew Ho <andyminhtuanho@gmail.com>
2023-04-22 03:49:06 -0700

    Added Coinbase Live Data Socket

--- App.js
+++ App.js
@@ -1,10 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar, setStatusBarBackgroundColor } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [webSocket, setWebSocket] = useState();
+
+  const [curPrice, setCurPrice] = useState();
+  useEffect(() => {
+    const connectCoinbaseStreaming = (from_ticker, to_ticker) => {
+      const ws = new WebSocket('wss://ws-feed.exchange.coinbase.com');
+      const conversion_string = `${from_ticker}-${to_ticker}`;
+
+      ws.onopen = () => {
+        ws.send(
+          JSON.stringify({
+            type: 'subscribe',
+            product_ids: [conversion_string],
+            channels: [
+              'level2',
+              'heartbeat',
+              {
+                name: 'ticker',
+                product_ids: [conversion_string],
+              },
+            ],
+          })
+        );
+      };
+      ws.onmessage = (e) => {
+        const data = JSON.parse(e.data);
+        console.log(data);
+        if (data && data.changes) {
+          const price = data.changes[0][1];
+          console.log(price);
+          setCurPrice(price);
+        }
+      };
+
+      ws.onerror = (error) => {
+        console.log(`WebSocket Error: ${error}`);
+      };
+
+      return ws;
+    };
+
+    const ws = connectCoinbaseStreaming('ETH', 'USD');
+
+    setWebSocket(ws);
+
+    return () => ws.close();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: 'black' }}>{curPrice}</Text>
       <StatusBar style="auto" />
     </View>
   );
