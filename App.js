0b480472a29157dafa0e450d440fd0da610faa26
Andrew Ho <andyminhtuanho@gmail.com>
2023-04-22 04:24:24 -0700

    Added historical data fetch functionality

--- App.js
+++ App.js
@@ -2,37 +2,41 @@ import { StatusBar, setStatusBarBackgroundColor } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+async function getHistoricalData(fromTicker, toTicker) {
+  const product_id = `${fromTicker}-${toTicker}`;
+  const response =
+    await fetch(`https://api.pro.coinbase.com/products/${product_id}/candles
+  `);
+  const data = await response.json();
+  return await data;
+}
+
 export default function App() {
   const [webSocket, setWebSocket] = useState();
-
   const [curPrice, setCurPrice] = useState();
+  const [histPrices, setHistPrices] = useState();
+  const [fromTicker, setFromTicker] = useState('ETH');
+  const [toTicker, setToTicker] = useState('USD');
+
   useEffect(() => {
-    const connectCoinbaseStreaming = (from_ticker, to_ticker) => {
+    const connectCoinbaseStreaming = (fromTicker, toTicker) => {
       const ws = new WebSocket('wss://ws-feed.exchange.coinbase.com');
-      const conversion_string = `${from_ticker}-${to_ticker}`;
+      const conversion_string = `${fromTicker}-${toTicker}`;
 
       ws.onopen = () => {
         ws.send(
           JSON.stringify({
             type: 'subscribe',
             product_ids: [conversion_string],
-            channels: [
-              'level2',
-              'heartbeat',
-              {
-                name: 'ticker',
-                product_ids: [conversion_string],
-              },
-            ],
+            channels: ['ticker'],
           })
         );
       };
       ws.onmessage = (e) => {
         const data = JSON.parse(e.data);
-        console.log(data);
-        if (data && data.changes) {
-          const price = data.changes[0][1];
-          console.log(price);
+        // console.log(data);
+        if (data && data.price) {
+          const price = data.price;
           setCurPrice(price);
         }
       };
@@ -41,11 +45,17 @@ export default function App() {
         console.log(`WebSocket Error: ${error}`);
       };
 
+      const product_id = `${fromTicker}-${toTicker}`;
+      const response =
+        fetch(`https://api.pro.coinbase.com/products/${product_id}/candles
+    `)
+          .then((res) => res.json())
+          .then((data) => setHistPrices(data));
+
       return ws;
     };
 
-    const ws = connectCoinbaseStreaming('ETH', 'USD');
-
+    const ws = connectCoinbaseStreaming(fromTicker, toTicker);
     setWebSocket(ws);
 
     return () => ws.close();
